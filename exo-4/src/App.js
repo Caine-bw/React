8229fa9e0ceb87660bea47b818625ab17cbdff3a
Abdel <elb.abd@icloud.com>
2021-03-31 14:42:51 +0200

    yeah bruh

--- exo-4/src/App.js
+++ exo-4/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="text-center">
+            <Button className="m-5 bg-danger">
+                Home! <FontAwesomeIcon icon={faHome} />
+            </Button>
+        </div>
+    );
 }
-
-export default App;
